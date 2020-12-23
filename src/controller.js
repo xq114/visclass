8abd7645b7292969a9e7d1663d4c835ade42ae25
xq114 <1140735506@qq.com>
2020-12-23 11:36:02 +0800

    a working version

--- src/controller.js
+++ src/controller.js
@@ -1,19 +1,25 @@
 import { TimeSpan } from "./timespan";
 import { StackedArea } from "./stacked-area";
 import { ScatterPlot } from "./scatter-plot";
-import { forceDirected } from "./force-directed";
+import { ForceDirected } from "./force-directed";
 
 function run(faculties, flow) {
   let ts = new TimeSpan("#p1", faculties);
-  let sa = new StackedArea("#p4");
+  let fd = new ForceDirected("#p2");
   let sp = new ScatterPlot("#p3");
-  let fd = new forceDirected("#p2");
+  let sa = new StackedArea("#p4");
 
   ts.draw_line();
   ts.draw_circles();
-  fd.init(flow);
   sa.init(faculties);
   sp.init(faculties);
+  fd.init(flow);
+  fd.set_listener(
+    (d) =>
+      (document.getElementById("l3").innerHTML = `
+    <h5>School Name</h5><p>${d.id}</p>
+    `)
+  );
 
   ts.set_listener(() => {
     let new_data = faculties.filter((d) => {
@@ -23,7 +29,9 @@ function run(faculties, flow) {
     sa.update(new_data, ts.start, ts.end);
     sp.update(new_data);
   });
-  sp.set_listener(d => document.getElementById("l3").innerHTML = `
+  sp.set_listener(
+    (d) =>
+      (document.getElementById("l3").innerHTML = `
     <h5>Name</h5><p>${d["First Name"]} ${d["Last Name"]}</p>
     <h5>Institution</h5><p>${d["Institution"]}</p>
     <h5>Research Interest</h5><p>${d["Research Interest"]}</p>
@@ -31,7 +39,8 @@ function run(faculties, flow) {
     <h5>H-index</h5><p>${d["H-index"]}</p>
     <h5>Citations</h5><p>${d["Citations"]}</p>
     <h5>Ph.D. Graduate School</h5><p>${d["Ph.D. Graduate School"]}</p>
-  `);
+  `)
+  );
 }
 
 export { run };
--- src/force-directed.js
+++ src/force-directed.js
@@ -1,7 +1,7 @@
 import * as d3 from "d3";
 import { getActualDim } from "./utils";
 
-function forceDirected(selector) {
+function ForceDirected(selector) {
   this.selector = selector;
   [this.width, this.height] = getActualDim(selector);
   this.svg = d3
@@ -9,12 +9,37 @@ function forceDirected(selector) {
     .attr("viewBox", [0, 0, this.width, this.height]);
 }
 
-forceDirected.prototype.init = function (data) {
+ForceDirected.prototype.set_listener = function (func) {
+  this.showInfo = func;
+  this.svg.selectAll("circle").on("mouseover", (e, d) => {
+    this.showInfo(d);
+    this.svg
+      .selectAll("circle")
+      .attr("fill", (f) => (d.id == f.id ? "var(--bs-yellow)" : "var(--bs-blue)"));
+  })
+};
+
+ForceDirected.prototype.init = function (data) {
   const [width, height] = [this.width, this.height];
-  const margin = { top: 10, right: 10, bottom: 20, left: 40 };
   const svg = this.svg;
   let links = data.links;
   let nodes = data.nodes;
+
+  for (const d of nodes) {
+    d.x = width / 2 + (Math.random() - 0.5) * 10;
+    d.y = height / 2 + (Math.random() - 0.5) * 10;
+  }
+
+  const simulation = d3
+    .forceSimulation()
+    .force("charge", d3.forceManyBody())
+    .force("collide", d3.forceCollide())
+    .force(
+      "link",
+      d3.forceLink().id((d) => d.id)
+    )
+    .force("x", d3.forceX(width * 0.4))
+    .force("y", d3.forceY(height * 0.4));
 
   let link = svg
     .append("g")
@@ -25,86 +50,54 @@ forceDirected.prototype.init = function (data) {
     .join("line")
     .attr("stroke-width", (d) => Math.sqrt(d.weight));
 
-  let node = svg
-    .append("g")
-    .attr("stroke", "#fff")
-    .attr("stroke-width", 0.5)
-    .selectAll("circle")
-    .data(nodes)
-    .join("circle")
-    .attr("r", (d) => Math.sqrt(d.weight) * 2 + 0.5)
-    .attr("fill", "steelblue")
-    .on("mouseover", function (e, d) {
-      text.attr("display", function (f) {
-        if (f.id == d.id || f.weight > 40) {
-          return "null";
-        } else {
-          return "none";
-        }
-      });
-    })
-    .on("mouseout", function (e, d) {
-      text.attr("display", function (f) {
-        if (f.weight > 40) {
-          return "null";
-        } else {
-          return "none";
-        }
-      });
-    })
-    .call(d3.drag().on("start", started).on("drag", dragged).on("end", ended));
-
-  let text = svg
-    .append("g")
-    .selectAll("text")
-    .data(nodes)
-    .join("text")
-    .text((d) => d.id)
-    .attr("display", function (d) {
-      if (d.weight > 40) {
-        return "null";
-      } else {
-        return "none";
-      }
-    });
-  const simulation = d3
-    .forceSimulation(nodes)
-    .force("charge", d3.forceManyBody())
-    .force("link", d3.forceLink(links))
-    .force("x", d3.forceX(width / 2))
-    .force("y", d3.forceY(height / 2));
-
-  simulation.alphaDecay(0.05);
-  simulation.force("charge").strength(-50);
-  simulation
-    .force("link")
-    .id((d) => d.id)
-    .distance(0)
-    .strength(1)
-    .iterations(1);
-
-  function started(d) {
-    if (!d3.event.active) {
+  function started(e, d) {
+    if (!e.active) {
       simulation.alphaTarget(0.2).restart();
     }
     d.fx = d.x;
     d.fy = d.y;
   }
 
-  function dragged(d) {
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
+  function dragged(e, d) {
+    d.fx = e.x;
+    d.fy = e.y;
   }
 
-  function ended(d) {
-    if (!d3.event.active) {
+  function ended(e, d) {
+    if (!e.active) {
       simulation.alphaTarget(0);
     }
     d.fx = null;
     d.fy = null;
   }
 
-  simulation.on("tick", ticked);
+  let node = svg
+    .append("g")
+    .selectAll("circle")
+    .data(nodes)
+    .join("circle")
+    .attr("stroke", "#fff")
+    .attr("stroke-width", 0.2)
+    .attr("r", (d) => Math.min(Math.sqrt(d.weight) * 2 + 0.5, 4 + Math.log(d.weight)))
+    .attr("fill", "var(--bs-blue)")
+    .on("mouseover", function (e, d) {
+      svg
+        .selectAll("circle")
+        .attr("fill", (f) => (d.id == f.id ? "var(--bs-yellow)" : "var(--bs-blue)"));
+    })
+    .on("mouseout", function (e, d) {
+      svg.selectAll("circle").attr("fill", "var(--bs-blue)");
+    })
+    .call(d3.drag().on("start", started).on("drag", dragged).on("end", ended));
+
+  simulation.nodes(nodes);
+  simulation.force("link").links(links).distance(0).strength(1).iterations(1);
+
+  simulation.alphaDecay(0.05);
+  simulation.force("charge").strength(-100);
+  simulation.force("x").strength(0.5);
+  simulation.force("y").strength(0.5);
+  simulation.force("collide").radius(10);
 
   function ticked() {
     link
@@ -115,6 +108,7 @@ forceDirected.prototype.init = function (data) {
 
     node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
   }
+  simulation.on("tick", ticked);
 };
 
-export { forceDirected };
+export { ForceDirected };

ccbb861e5c7749354cf321e7d12bcdf7e8a4f19c
jhcsss <59335224+jhcsss@users.noreply.github.com>
2020-12-23 08:14:33 +0800

    Update force-directed.js

--- src/force-directed.js
+++ src/force-directed.js
@@ -1,4 +1,6 @@
 import * as d3 from 'd3'
+<script src="https://d3js.org/d3.v5.min.js"></script>
+<script src="https://d3js.org/d3-force.v1.min.js"></script>
 let _width = 1000;
 let _height = 1000;
 let width = _width;
@@ -7,98 +9,15 @@ let height = _height;
 let data = null;
 let data_file = './data/data.json';
 
-    const svg = d3.select('body')
+ function draw_graph(){
+    const svg = d3.select('#container')
         .append('svg')
         .attr('width', width)
         .attr('height', height)
         .attr('class', 'chart')
-
-    const simulation = d3.forceSimulation(nodes)
-        .force('charge', d3.forceManyBody())
-        .force('link', d3.forceLink(links))
-        .force('x', d3.forceX(width / 2))
-        .force('y', d3.forceY(height / 2))
-
-    simulation.alphaDecay(0.05)
-    simulation.force('charge')
-        .strength(-50)
-    simulation.force('link')
-        .id(d => d.id) 
-        .distance(0) 
-        .strength(1) 
-        .iterations(1) 
-
-    const simulationLinks = svg.append('g')
-        .selectAll('line')
-        .data(links)
-        .enter()
-        .append('line')
-        .attr('stroke', d => '#c2c2c2')
-
-    const simulationNodes = svg.append('g')
-        .attr('fill', '#fff')
-        .attr('stroke', '#000')
-        .attr('stroke-width', 1.5)
-        .selectAll('circle')
-        .data(nodes)
-        .enter()
-        .append('circle')
-        .attr('r', 3.5)
-        .attr('fill', d => d.children ? null : '#000') 
-        .attr('stroke', d => d.children ? null : '#fff')
-        .call(d3.drag()
-            .on('start', started)
-            .on('drag', dragged)
-            .on('end', ended)
-        )
-
-    function started(d) {
-        if (!d3.event.active) {
-            simulation.alphaTarget(.2).restart()
-        }
-        d.fx = d.x 
-        d.fy = d.y
-    }
-
-    function dragged(d) {
-        d.fx = d3.event.x
-        d.fy = d3.event.y
-    }
-
-    function ended(d) {
-        if (!d3.event.active) {
-            simulation.alphaTarget(0)
-        }
-        d.fx = null
-        d.fy = null
-    }
-
-    simulation.on('tick', ticked)
-
-    function ticked() {
-        simulationLinks.attr('x1', d => d.source.x)
-            .attr('y1', d => d.source.y)
-            .attr('x2', d => d.target.x)
-            .attr('y2', d => d.target.y)
-
-        simulationNodes.attr('cx', d => d.x)
-            .attr('cy', d => d.y)
-    }
-
-
-
-    let svg = d3.select('#container')
-        .select('svg')
-        .attr('width', width)
-        .attr('height', height);
-
+   
     let links = data.links;
     let nodes = data.nodes;
-
-    for (i in nodes) {
-    	nodes_idx_dict[nodes[i].id] = i;
-        nodes_dict[nodes[i].id] = nodes[i];
-    }
 
     let link = svg.append("g")
         .attr("stroke", "#999")
@@ -152,28 +71,62 @@ let data_file = './data/data.json';
                 return 'none';
             }
         });
+    const simulation = d3.forceSimulation(nodes)
+        .force('charge', d3.forceManyBody())
+        .force('link', d3.forceLink(links))
+        .force('x', d3.forceX(width / 2))
+        .force('y', d3.forceY(height / 2))
 
-    graph_layout_algorithm(nodes, links)
+    simulation.alphaDecay(0.05)
+    simulation.force('charge')
+        .strength(-50)
+    simulation.force('link')
+        .id(d => d.id) 
+        .distance(0) 
+        .strength(1) 
+        .iterations(1) 
 
-    link
-        .attr("x1", d => nodes_dict[d.source].x)
-        .attr("y1", d => nodes_dict[d.source].y)
-        .attr("x2", d => nodes_dict[d.target].x)
-        .attr("y2", d => nodes_dict[d.target].y);
+    
 
-    node
-        .attr("cx", d => d.x)
-        .attr("cy", d => d.y);
-    text
-        .attr("x", d => d.x)
-        .attr("y", d => d.y)
-}
+    function started(d) {
+        if (!d3.event.active) {
+            simulation.alphaTarget(.2).restart()
+        }
+        d.fx = d.x 
+        d.fy = d.y
+    }
 
-function main() {
+    function dragged(d) {
+        d.fx = d3.event.x
+        d.fy = d3.event.y
+    }
+
+    function ended(d) {
+        if (!d3.event.active) {
+            simulation.alphaTarget(0)
+        }
+        d.fx = null
+        d.fy = null
+    }
+
+    simulation.on('tick', ticked)
+
+    function ticked() {
+       link.attr('x1', d => d.source.x)
+            .attr('y1', d => d.source.y)
+            .attr('x2', d => d.target.x)
+            .attr('y2', d => d.target.y)
+
+        node.attr('cx', d => d.x)
+            .attr('cy', d => d.y)
+    }
+
+
+ }
+   
+
     d3.json(data_file).then(function (DATA) {
         data = DATA;
         draw_graph();
-    })
-}
-
-main()
+    }
+export as{force-directed}
